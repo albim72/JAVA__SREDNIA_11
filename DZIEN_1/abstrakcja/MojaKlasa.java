05aaaea07ff581115a4e9149ad5442331dca337a
albim72 <marcin.albiniak72@gmail.com>
2024-11-19 12:09:45 +0100

    Create MojaKlasa.java

---
+++ DZIEN_1/abstrakcja/MojaKlasa.java
@@ -0,0 +1,18 @@
+public class MojaKlasa extends Prototyp {
+    int s;
+
+    public MojaKlasa(int x, int y, int s) {
+        super(x, y);
+        this.s = s;
+    }
+
+    @Override
+    public int policz() {
+        return (x+y)*s;
+    }
+
+    @Override
+    public void msg() {
+        System.out.println("to jest metoda abstrakcyjna przekazana z klasy Ekstras!");
+    }
+}
