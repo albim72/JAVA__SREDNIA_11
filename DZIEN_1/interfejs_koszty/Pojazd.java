f4fa122368c6973646b7dd9e0ff18f4d2b419f94
albim72 <marcin.albiniak72@gmail.com>
2024-11-20 08:50:50 +0100

    Update Pojazd.java

--- DZIEN_1/interfejs_koszty/Pojazd.java
+++ DZIEN_1/interfejs_koszty/Pojazd.java
@@ -1,4 +1,4 @@
-public class Pojazd implements IPojazd {
+public class Pojazd implements IPojazd,IComment {
     @Override
     public double spalanie100(double odl, double litry) {
         return litry*100/odl;
@@ -7,5 +7,10 @@ public class Pojazd implements IPojazd {
     @Override
     public double kosztyPrzejazdu(double odl, double litry, double cenal) {
         return litry*cenal+odl*0.01;
+    }
+
+    @Override
+    public String info(String msg) {
+        return "Bardzo ważna wiadmośc: " + msg;
     }
 }
