4c550f667da8d5da72bc16f6d73e926fd82d9765
albim72 <marcin.albiniak72@gmail.com>
2024-11-20 16:22:39 +0100

    Create dynamiczneobject.java

---
+++ DZIEN_2/dynamiczneobject.java
@@ -0,0 +1,14 @@
+import java.util.Objects;
+
+public class Main {
+    public static void main(String[] args) {
+        Object obj = "biały śnieg!";
+        System.out.println(obj);
+
+        obj = 42;
+        System.out.println(obj);
+
+//        String str = (String) obj;
+
+    }
+}
