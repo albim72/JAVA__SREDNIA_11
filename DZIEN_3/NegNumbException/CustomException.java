14af9ff5373ccd5a054619ddbc1f9dbe94642765
albim72 <marcin.albiniak72@gmail.com>
2024-11-21 13:54:29 +0100

    Update CustomException.java

--- DZIEN_3/NegNumbException/CustomException.java
+++ DZIEN_3/NegNumbException/CustomException.java
@@ -1,13 +1,13 @@
 public class CustomException extends Exception{
-    private final int errorCode;
-    private final String details;
+    private int errorCode = 0;
+    private String details = "";
 
     public CustomException(String message, int errorCode, String details) {
         super(message);
         this.errorCode = errorCode;
         this.details = details != null ? details:"Brak dodatkowych informacji!";
     }
-    
+
     public CustomException(String message){
         super(message);
     }
