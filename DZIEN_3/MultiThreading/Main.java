399956ff5da06c5aa43294be15c9434bfb72e8ae
albim72 <marcin.albiniak72@gmail.com>
2024-11-21 14:10:52 +0100

    Create Main.java

---
+++ DZIEN_3/MultiThreading/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int n=8;
+        for (int i=0; i<n; i++){
+            MyThread object = new MyThread();
+            object.start();
+        }
+    }
+}
