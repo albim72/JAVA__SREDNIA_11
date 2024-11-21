0af2123dce9fdc3d20cced5fb669773481647e56
albim72 <marcin.albiniak72@gmail.com>
2024-11-21 14:06:37 +0100

    Create MyThread.java

---
+++ DZIEN_3/MultiThreading/MyThread.java
@@ -0,0 +1,10 @@
+public class MyThread extends Thread{
+    @Override
+    public void run() {
+        try{
+            System.out.println("Wątek " + Thread.currentThread().getId() + " jest uruchomiony!");
+        } catch (Exception e){
+            System.out.println("problem z wątkiem: " + e);
+        }
+    }
+}
